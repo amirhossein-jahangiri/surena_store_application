d33db01a959c8a052990ac08d3df30e74abec17a
AmirHossein Jahangiri <amirhosseinjahangiri78@gmail.com>
2021-12-28 12:44:06 +0330

    update theme config file for config theme, styles, colors and etc for this application

---
+++ lib/config/theme_config.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/material.dart';
+import 'package:sizer/sizer.dart';
+
+// todo this class for theme , colors , fonts and etc in this application
+
+class ThemeConfig {
+  static const String fontFamily = 'IranSans';
+  static const Color _lightScaffoldBackColor = Colors.white;
+  static const Color _lightPrimaryColor = Colors.purple;
+  static const Color _lightOnPrimaryColor = Colors.white;
+  static const Color _lightPrimaryVariantColor = Colors.purpleAccent;
+
+  static ColorScheme lightColorScheme = const ColorScheme.light(
+    primary: _lightPrimaryColor,
+    onPrimary: _lightOnPrimaryColor,
+    primaryVariant: _lightPrimaryVariantColor,
+  );
+
+  static TextTheme lightTextTheme = TextTheme(
+    bodyText1: lightBodyText1Style,
+    bodyText2: lightBodyText2Style,
+    button: lightButtonStyle,
+  );
+
+  static TextStyle lightBodyText1Style = TextStyle(
+    fontWeight: FontWeight.bold,
+    fontSize: 15.sp,
+    letterSpacing: 0.75,
+    color: _lightPrimaryColor,
+  );
+  static TextStyle lightBodyText2Style = TextStyle(
+    fontSize: 15.sp,
+    letterSpacing: 0.5,
+    color: _lightPrimaryColor,
+  );
+  static TextStyle lightButtonStyle = TextStyle(
+    fontSize: 14.sp,
+    letterSpacing: 0.75,
+    color: _lightPrimaryColor,
+  );
+
+  static IconThemeData lightIconTheme = IconThemeData(
+    color: _lightPrimaryVariantColor,
+    size: 10.sp,
+  );
+
+  static final ThemeData lightTheme = ThemeData(
+    scaffoldBackgroundColor: _lightScaffoldBackColor,
+    fontFamily: fontFamily,
+    splashColor: _lightPrimaryColor,
+    colorScheme: lightColorScheme,
+    textTheme: lightTextTheme,
+    iconTheme: lightIconTheme,
+  );
+
+}
