c7896b11aebc8ea468a0c011ec6750928bd68873
AmirHossein Jahangiri <amirhosseinjahangiri78@gmail.com>
2021-12-29 14:45:40 +0330

    make app constants class for all of the strings in my app

---
+++ lib/constants/app_constants.dart
@@ -0,0 +1,7 @@
+
+// todo this class for write application constatns like that text
+
+class AppConstants {
+  // todo constants for product overview screen
+  static const String PRODUCT_OVERVIEW_SCREEN_APP_BAR_TITLE = 'جستجو در سورنا';
+}
