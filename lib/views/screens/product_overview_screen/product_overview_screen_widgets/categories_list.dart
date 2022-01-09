475d4b5013c9b7cb439b0ccd83b503b77faeecd5
AmirHossein Jahangiri <amirhosseinjahangiri78@gmail.com>
2022-01-09 23:20:21 +0330

    adding this widget for show list of categories in product overview screen and separate it

---
+++ lib/views/screens/product_overview_screen/product_overview_screen_widgets/categories_list.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+import 'package:sizer/sizer.dart';
+
+// todo this widget for list of categories in product overview screens (purple circle container)
+
+class CategoriesList extends StatelessWidget {
+  const CategoriesList({Key? key,}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: 14.h,
+      child: ListView.builder(
+        itemCount: 10,
+        shrinkWrap: true,
+        scrollDirection: Axis.horizontal,
+        itemBuilder: (context, index) {
+          return Column(
+            children: [
+              Container(
+                margin: const EdgeInsets.symmetric(horizontal: 10),
+                height: 8.h,
+                width: 20.w,
+                decoration: BoxDecoration(
+                  shape: BoxShape.circle,
+                  color: Theme.of(context).colorScheme.primary,
+                ),
+                child: Icon(Icons.security, size: 5.h, color: Theme.of(context).colorScheme.onPrimary,),
+              ),
+              Text(
+                'item $index',
+                style: Theme.of(context).textTheme.button,
+              ),
+            ],
+          );
+        },
+      ),
+    );
+  }
+}
