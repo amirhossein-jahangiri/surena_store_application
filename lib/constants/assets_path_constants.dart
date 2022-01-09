d8b3ad31382fba9f09241b8077efa1cd4963e54b
AmirHossein Jahangiri <amirhosseinjahangiri78@gmail.com>
2022-01-09 23:15:37 +0330

    adding a assets path constants class to this file for write assets path and use it in another screens and update it

---
+++ lib/constants/assets_path_constants.dart
@@ -0,0 +1,11 @@
+
+// todo this class file for write assets in this class and use it in another screens
+
+class AssetsPathConstants {
+
+  // todo this assets for product overview screens
+  static const String SLIDE_ONE_ASSETS = 'http://s6.uplod.ir/i/01032/y63y6uwmhmr6.png';
+  static const String SLIDE_TWO_ASSETS = 'http://s6.uplod.ir/i/01032/v1w9uuo0mxz5.png';
+  static const String SLIDE_THREE_ASSETS = 'http://s6.uplod.ir/i/01032/jgajhjaliilc.png';
+  static const String SLIDE_FOUR_ASSETS = 'http://s6.uplod.ir/i/01032/xqe3n2fv2mce.png';
+}
