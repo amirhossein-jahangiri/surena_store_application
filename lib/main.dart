d7ac7c322f2e9bfdd684b421fd862278becc3abc
AmirHossein Jahangiri <amirhosseinjahangiri78@gmail.com>
2022-01-09 23:17:27 +0330

    update main and adding multiprovider widget to this file with imageSlider provider model for product overview screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,28 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import 'package:sizer/sizer.dart';
 
 import '/config/theme_config.dart';
 import '/config/route_config.dart';
 import '/constants/route_constants.dart';
+import 'modules/provider_models/product_overview_screen_slider_provider_model.dart';
 
-void main() => runApp(MyApp());
+void main() => runApp(
+      MultiProvider(
+        providers: [
+          ChangeNotifierProvider(create: (context) => ProductOverViewScreenSliderProviderModel()),
+        ],
+        child: const MyApp(),
+      ),
+    );
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Sizer( // todo for sizer package and make responsive application
+    return Sizer(
+      // todo for sizer package and make responsive application
       builder: (context, orientation, deviceType) {
         return MaterialApp(
           debugShowCheckedModeBanner: false,
