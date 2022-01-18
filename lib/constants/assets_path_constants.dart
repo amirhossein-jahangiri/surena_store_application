abf290a7d3ea3d1e0713e5a7afe1dc09a49bcb50
ghazal-moradi <ghazal.moradi.github@gmail.com>
2022-01-18 22:44:44 +0330

    updated this file and adding recommended image for amazing products and amazing banner

--- lib/constants/assets_path_constants.dart
+++ lib/constants/assets_path_constants.dart
@@ -8,4 +8,6 @@ class AssetsPathConstants {
   static const String SLIDE_TWO_ASSETS = 'http://s6.uplod.ir/i/01032/v1w9uuo0mxz5.png';
   static const String SLIDE_THREE_ASSETS = 'http://s6.uplod.ir/i/01032/jgajhjaliilc.png';
   static const String SLIDE_FOUR_ASSETS = 'http://s6.uplod.ir/i/01032/xqe3n2fv2mce.png';
+  static const String RECOMMENDED_PRODUCTS_IMAGE_ASSET = 'http://s6.uplod.ir/i/01033/tx1x3yd0ln5h.png';
+  static const String AMAZING_OFFER_BANNER_SVG = 'assets/icons/offer.svg';
 }
