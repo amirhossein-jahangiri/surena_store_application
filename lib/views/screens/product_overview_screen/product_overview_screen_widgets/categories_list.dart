4fa67b7dd73c5b68026d56062d0dec6dea3d5066
ghazal-moradi <ghazal.moradi.github@gmail.com>
2022-01-18 22:45:40 +0330

    updated this file and decrease size of circle container for display categories list

--- lib/views/screens/product_overview_screen/product_overview_screen_widgets/categories_list.dart
+++ lib/views/screens/product_overview_screen/product_overview_screen_widgets/categories_list.dart
@@ -9,7 +9,7 @@ class CategoriesList extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return SizedBox(
-      height: 14.h,
+      height: 11.h,
       child: ListView.builder(
         itemCount: 10,
         shrinkWrap: true,
@@ -19,13 +19,13 @@ class CategoriesList extends StatelessWidget {
             children: [
               Container(
                 margin: const EdgeInsets.symmetric(horizontal: 10),
-                height: 8.h,
+                height: 6.h,
                 width: 20.w,
                 decoration: BoxDecoration(
                   shape: BoxShape.circle,
                   color: Theme.of(context).colorScheme.primary,
                 ),
-                child: Icon(Icons.security, size: 5.h, color: Theme.of(context).colorScheme.onPrimary,),
+                child: Icon(Icons.security, size: 4.h, color: Theme.of(context).colorScheme.onPrimary,),
               ),
               Text(
                 'item $index',
