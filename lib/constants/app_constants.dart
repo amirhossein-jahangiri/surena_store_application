6c062f01acf80c6b3fd4bd3aa1a8516cfca4688f
ghazal-moradi <ghazal.moradi.github@gmail.com>
2022-01-18 22:43:55 +0330

    adding amazaing offer constants to this file

--- lib/constants/app_constants.dart
+++ lib/constants/app_constants.dart
@@ -4,4 +4,6 @@
 class AppConstants {
   // todo constants for product overview screen
   static const String PRODUCT_OVERVIEW_SCREEN_APP_BAR_TITLE = 'جستجو در سورنا';
+  static const String AMAZING_OFFER_TEXT = 'پیشنهاد های شگفت انگیز';
+  static const String AMAZING_OFFER_SEE_ALL_PRODUCTS_TEXT = 'مشاهده همه';
 }
