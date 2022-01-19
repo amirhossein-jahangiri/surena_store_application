665b63ec9d69eea364ba20da6c5d0598a69af6a5
Amirhossein-Jahangiri <amirhosseinjahangiri78@gmail.com>
2022-01-19 20:40:38 +0330

    update application constants file and adding some const variable for tabs screen

--- lib/constants/app_constants.dart
+++ lib/constants/app_constants.dart
@@ -2,6 +2,13 @@
 // todo this class for write application constatns like that text
 
 class AppConstants {
+
+  // todo constants for tab controller screen
+  static const String HOME = 'خانه';
+  static const String CATEGORIES = 'دسته بندی';
+  static const String CART = 'سبد خرید';
+  static const String PROFILE = 'پروفایل';
+
   // todo constants for product overview screen
   static const String PRODUCT_OVERVIEW_SCREEN_APP_BAR_TITLE = 'جستجو در سورنا';
   static const String AMAZING_OFFER_TEXT = 'پیشنهاد های شگفت انگیز';
