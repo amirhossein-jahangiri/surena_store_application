828f1d12eee647357b297f5e0c9ad5646072c80c
Amirhossein-Jahangiri <amirhosseinjahangiri78@gmail.com>
2022-01-19 20:38:15 +0330

    update this constants class and adding tabs screen, shopping cart, user profile, categories list constants route name

--- lib/constants/route_constants.dart
+++ lib/constants/route_constants.dart
@@ -2,5 +2,9 @@
 // todo this class for route name constants
 
 class RouteConstants {
+  static const String TABS_SCREEN_ROUTE_NAME = '/';
   static const String PRODUCT_OVERVIEW_SCREEN_ROUTE_NAME = '/product-overview';
+  static const String CATEGORIES_SCREEN_ROUTE_NAME = '/categories';
+  static const String SHOPPING_CART_SCREEN_ROUTE_NAME = '/shopping-cart';
+  static const String USER_PROFILE_SCREEN_ROUTE_NAME = '/user-profile';
 }
