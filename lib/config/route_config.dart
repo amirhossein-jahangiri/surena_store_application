1364e5377fb9a375ecb6592f980890f66e62df09
Amirhossein-Jahangiri <amirhosseinjahangiri78@gmail.com>
2022-01-19 20:39:11 +0330

    update this route config class file and adding shopping cart, user profile, categories list and tabs screen route constants to this config file

--- lib/config/route_config.dart
+++ lib/config/route_config.dart
@@ -1,10 +1,17 @@
 import 'package:flutter/material.dart';
-import '/constants/route_constants.dart';
 
+import '/constants/route_constants.dart';
 import '/views/screens/product_overview_screen/product_overview_screen.dart';
+import '/views/screens/tabs_screen/tabs_screen.dart';
+import '/views/screens/categories_screen/categories_screen.dart';
+import '/views/screens/user_profile_screen/user_profile_screen.dart';
 
 class RouteConfig {
   static Map<String, Widget Function(BuildContext)> routes = {
+    RouteConstants.TABS_SCREEN_ROUTE_NAME: (context) => TabsScreen(),
     RouteConstants.PRODUCT_OVERVIEW_SCREEN_ROUTE_NAME: (context) => ProductOverViewScreen(),
+    RouteConstants.CATEGORIES_SCREEN_ROUTE_NAME: (context) => CategoriesScreen(),
+    RouteConstants.SHOPPING_CART_SCREEN_ROUTE_NAME: (context) => CategoriesScreen(),
+    RouteConstants.USER_PROFILE_SCREEN_ROUTE_NAME: (context) => UserProfileScreen(),
   };
 }
