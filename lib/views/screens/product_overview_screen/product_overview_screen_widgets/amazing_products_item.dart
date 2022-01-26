149b2f4dc62994602f08be477dca52267c4bcce1
Amirhossein-Jahangiri <amirhosseinjahangiri78@gmail.com>
2022-01-26 14:12:55 +0330

    make a separate file for display amazing product item and use it in amazing products list widget and dart file and update it

---
+++ lib/views/screens/product_overview_screen/product_overview_screen_widgets/amazing_products_item.dart
@@ -0,0 +1,86 @@
+import 'package:flutter/material.dart';
+import 'package:sizer/sizer.dart';
+import 'package:cached_network_image/cached_network_image.dart';
+
+import '/views/screens/product_overview_screen/product_overview_screen_widgets/product_price_with_discount.dart';
+
+// todo display this widget for show items in amazing products list on product overview screen
+
+class AmazingProductsItem extends StatelessWidget {
+  final double? height;
+  final double? width;
+  final String? image;
+  final String? title;
+  final String? price;
+  final String? discountPercent;
+
+  const AmazingProductsItem({
+    this.height,
+    this.width,
+    this.image,
+    this.title,
+    this.price,
+    this.discountPercent,
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: height,
+      width: width,
+      margin: EdgeInsets.symmetric(
+        vertical: 3.h,
+        horizontal: 3.w,
+      ),
+      padding: EdgeInsets.all(8.sp),
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(8.sp),
+        color: Theme.of(context).colorScheme.onPrimary,
+      ),
+      child: Column(
+        children: [
+          const Spacer(),
+
+          // todo display image of product
+          CachedNetworkImage(
+            imageUrl: image!,
+            height: 20.h,
+            fit: BoxFit.contain,
+            progressIndicatorBuilder: (context, url, progress) {
+              return Center(
+                child: CircularProgressIndicator(value: progress.progress),
+              );
+            },
+          ),
+
+          const Spacer(),
+
+          // todo display title of product
+          SizedBox(
+            width: double.infinity,
+            child: Text(
+              title!,
+              textAlign: TextAlign.right,
+              style: Theme.of(context).textTheme.bodyText2!.copyWith(
+                    color: Theme.of(context).colorScheme.secondary,
+                  ),
+            ),
+          ),
+
+          // todo display divider
+          Divider(
+            thickness: 2,
+            color: Theme.of(context).colorScheme.primary,
+          ),
+
+          // todo display price and discount percent
+          ProductPriceWithDiscount(
+            price: price!,
+            discountPercent: discountPercent!,
+          ),
+        ],
+      ),
+    );
+  }
+}
--- lib/views/screens/product_overview_screen/product_overview_screen_widgets/amazing_products_list.dart
+++ lib/views/screens/product_overview_screen/product_overview_screen_widgets/amazing_products_list.dart
@@ -1,9 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:sizer/sizer.dart';
-import 'package:cached_network_image/cached_network_image.dart';
 
+import 'amazing_products_item.dart';
 import '/constants/assets_path_constants.dart';
-import '../product_overview_screen_widgets/product_price_with_discount.dart';
 
 // todo this widget for display amazing products in product overview screen
 
@@ -19,71 +18,19 @@ class AmazingProductsList extends StatelessWidget {
       shrinkWrap: true,
       scrollDirection: Axis.horizontal,
       itemBuilder: (context, index) {
-        return Container(
+        return AmazingProductsItem( // todo display items and use amazing products item dart file
           height: 50.h,
           width: 50.w,
-          margin: EdgeInsets.symmetric(
-            vertical: 3.h,
-            horizontal: 3.w,
-          ),
-          padding: EdgeInsets.all(8.sp),
-          decoration: BoxDecoration(
-            borderRadius: BorderRadius.circular(8.sp),
-            color: Theme.of(context).colorScheme.onPrimary,
-          ),
-          child: Column(
-            children: [
-
-              const Spacer(),
-
-              // todo display image of product
-              CachedNetworkImage(
-                imageUrl: AssetsPathConstants
-                    .RECOMMENDED_PRODUCTS_IMAGE_ASSET,
-                height: 20.h,
-                fit: BoxFit.contain,
-                progressIndicatorBuilder:
-                    (context, url, progress) {
-                  return Center(
-                      child: CircularProgressIndicator(
-                          value: progress.progress));
-                },
-              ),
-
-              const Spacer(),
-
-              // todo display title of product
-              SizedBox(
-                width: double.infinity,
-                child: Text(
-                  'کیف زنانه مدل p',
-                  textAlign: TextAlign.right,
-                  style: Theme.of(context)
-                      .textTheme
-                      .bodyText2!
-                      .copyWith(
-                    color: Theme.of(context)
-                        .colorScheme
-                        .secondary,
-                  ),
-                ),
-              ),
-
-              // todo display divider
-              Divider(
-                thickness: 2,
-                color: Theme.of(context).colorScheme.primary,
-              ),
-
-              // todo display price and discount percent
-              const ProductPriceWithDiscount(),
-            ],
-          ),
+          image: AssetsPathConstants.RECOMMENDED_PRODUCTS_IMAGE_ASSET,
+          title: 'کیف زنانه مدل p',
+          price: '35000 تومان',
+          discountPercent: '10 %',
         );
       },
     );
   }
 }
+
 
 
 
