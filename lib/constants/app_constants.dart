196fdc2f46d418c5a7812fdbeeba115cf3dc520d
Amirhossein-Jahangiri <amirhosseinjahangiri78@gmail.com>
2022-01-29 15:37:34 +0330

    adding newly constants for use in product details screen

--- lib/constants/app_constants.dart
+++ lib/constants/app_constants.dart
@@ -14,4 +14,11 @@ class AppConstants {
   static const String AMAZING_OFFER_TEXT = 'پیشنهاد های شگفت انگیز';
   static const String AMAZING_OFFER_SEE_ALL_PRODUCTS_TEXT = 'مشاهده همه';
   static const String BEST_SELLING_PRODUCTS_TEXT = 'محصولات پر فروش';
+
+  // todo constants for product details screen
+  static const String ADD_TO_SHOPPING_CART_BTN_TEXT = 'افزودن به سبد خرید';
+  static const String BACK_ICON_BTN_TOOLTIP = 'برگشت';
+  static const String GO_TO_SHOPPING_CART_ICON_BTN_TOOLTIP = 'سبد خرید';
+  static const String ADD_PRODUCT_TO_FAVORITE_LIST_ICON_BTN_TOOLTIP = 'اضافه به لیست علاقه مندی ها';
+  static const String OPEN_MORE_LIST_ICON_BTN_TOOLTIP = 'یه چیزی';
 }
