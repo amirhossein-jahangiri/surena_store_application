b8855cfb0c3da5687a84ca31f994ac8dd3557f07
Amirhossein-Jahangiri <amirhosseinjahangiri78@gmail.com>
2022-01-29 15:38:23 +0330

    add a new dart file including icon button shopping cart and a text for show shopping cart list count for display to user

---
+++ lib/views/widgets/shopping_cart_badget.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+import 'package:sizer/sizer.dart';
+
+import '/constants/app_constants.dart';
+
+
+// todo display shopping cart badget and hold shopping cart list count and show for user
+
+
+class ShoppingCartBadget extends StatelessWidget {
+  final String? title;
+  final Function()? press;
+  const ShoppingCartBadget({this.title, this.press, Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      alignment: Alignment.center,
+      child: Stack(
+        children: [
+          IconButton(
+            onPressed: press,
+            icon: const Icon(Icons.shopping_cart),
+            iconSize: 3.h,
+            tooltip: AppConstants.ADD_TO_SHOPPING_CART_BTN_TEXT,
+          ),
+          Positioned(
+            right: 2,
+            child: Container(
+              padding: EdgeInsets.all(3.sp),
+              decoration: const BoxDecoration(
+                color: Colors.red,
+                shape: BoxShape.circle,
+              ),
+              //alignment: Alignment.center,
+              child: FittedBox(
+                alignment: Alignment.center,
+                child: Text(
+                  title!,
+                  style: Theme.of(context).textTheme.caption!.copyWith(
+                    color: Theme.of(context).colorScheme.onPrimary,
+                  ),
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
