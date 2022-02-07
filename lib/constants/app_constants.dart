ea0a3c166f18ef855a905ba996c7f325aed6b2b0
Amirhossein-Jahangiri <amirhosseinjahangiri78@gmail.com>
2022-02-07 16:40:16 +0330

    adding 3 line of string constants for text widget buy button updated and add product id and product category name

--- lib/constants/app_constants.dart
+++ lib/constants/app_constants.dart
@@ -13,10 +13,12 @@ class AppConstants {
   static const String PRODUCT_OVERVIEW_SCREEN_APP_BAR_TITLE = 'جستجو در سورنا';
   static const String AMAZING_OFFER_TEXT = 'پیشنهاد های شگفت انگیز';
   static const String AMAZING_OFFER_SEE_ALL_PRODUCTS_TEXT = 'مشاهده همه';
-  static const String BEST_SELLING_PRODUCTS_TEXT = 'محصولات پر فروش';
+  static const String BEST_SELLING_PRODUCTS_TEXT = 'محصولات پرفروش';
 
   // todo constants for product details screen
-  static const String ADD_TO_SHOPPING_CART_BTN_TEXT = 'افزودن به سبد خرید';
+  static const String DISPLAY_PRODUCT_CODE_TITLE = 'کد: ';
+  static const String DISPLAY_PRODUCT_CATEGORY_NAME_TITLE = 'در دسته ';
+  static const String ADD_TO_SHOPPING_CART_BTN_TEXT = 'افزودن';
   static const String BACK_ICON_BTN_TOOLTIP = 'برگشت';
   static const String GO_TO_SHOPPING_CART_ICON_BTN_TOOLTIP = 'سبد خرید';
   static const String ADD_PRODUCT_TO_FAVORITE_LIST_ICON_BTN_TOOLTIP = 'اضافه به لیست علاقه مندی ها';
