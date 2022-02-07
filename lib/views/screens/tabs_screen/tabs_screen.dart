b8ca39934f17b8265d6cdacbc32efcc6e765fcd7
Amirhossein-Jahangiri <amirhosseinjahangiri78@gmail.com>
2022-02-07 16:41:29 +0330

    nothing

--- lib/views/screens/tabs_screen/tabs_screen.dart
+++ lib/views/screens/tabs_screen/tabs_screen.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:sizer/sizer.dart';
 import 'package:provider/provider.dart';
 
 import '/modules/provider_models/app_controller_provider_model.dart';
