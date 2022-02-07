6e2999d7bb04a9c5398a87b5a8f8ae85cd8f9ee8
Amirhossein-Jahangiri <amirhosseinjahangiri78@gmail.com>
2022-02-07 16:42:40 +0330

    update and decrease font size update primary color to deep purple and adding a new color called surface color

--- lib/config/theme_config.dart
+++ lib/config/theme_config.dart
@@ -8,12 +8,14 @@ class ThemeConfig {
   static const String fontFamily = 'IranSans';
   static const String secondaryFont = 'Aviny';
   static const Color _lightScaffoldBackColor = Colors.white;
-  static const Color _lightPrimaryColor = Colors.purple;
+  //static const Color _lightPrimaryColor = Colors.purple;
+  static const Color _lightPrimaryColor = Color(0xFF6a1b9a);
   static const Color _lightOnPrimaryColor = Colors.white;
   static const Color _lightPrimaryVariantColor = Colors.purpleAccent;
   static const Color _lightSecondaryColor =  Colors.black;
   static const Color _lightOnSecondaryColor = Colors.grey;
   static const Color _lightErrorColor = Colors.red;
+  static const Color _lightSurfaceColor = Color(0xFFFAFAFA);
 
   static ColorScheme lightColorScheme = const ColorScheme.light(
     primary: _lightPrimaryColor,
@@ -22,9 +24,11 @@ class ThemeConfig {
     secondary: _lightSecondaryColor,
     onSecondary: _lightOnSecondaryColor,
     error: _lightErrorColor,
+    surface: _lightSurfaceColor,
   );
 
   static TextTheme lightTextTheme = TextTheme(
+    headline4: lightHeadline4TextStyle,
     bodyText1: lightBodyText1Style,
     bodyText2: lightBodyText2Style,
     button: lightButtonStyle,
@@ -32,34 +36,39 @@ class ThemeConfig {
     overline: lightOverLineStyle,
   );
 
+  static TextStyle lightHeadline4TextStyle = TextStyle(
+    fontSize: 16.sp, // 17
+    letterSpacing: 0.75,
+    color: _lightSecondaryColor,
+  );
   static TextStyle lightBodyText1Style = TextStyle(
     fontWeight: FontWeight.bold,
-    fontSize: 15.sp,
+    fontSize: 14.sp, // 15
     letterSpacing: 0.75,
     color: _lightPrimaryColor,
   );
   static TextStyle lightBodyText2Style = TextStyle(
-    fontSize: 15.sp,
+    fontSize: 14.sp, // 15
     letterSpacing: 0.5,
     color: _lightPrimaryColor,
   );
   static TextStyle lightButtonStyle = TextStyle(
-    fontSize: 13.sp,
+    fontSize: 12.sp, // 13
     color: _lightPrimaryColor,
   );
   static TextStyle lightSubTitle2Style = TextStyle(
-    fontSize: 11.sp,
+    fontSize: 10.sp, // 11
     color: _lightOnSecondaryColor,
   );
   static TextStyle lightOverLineStyle = TextStyle(
-    fontSize: 15.sp,
+    fontSize: 14.sp, // 15
     fontFamily: secondaryFont,
     color: _lightOnSecondaryColor,
   );
 
   static IconThemeData lightIconTheme = IconThemeData(
     color: _lightPrimaryVariantColor,
-    size: 10.h,
+    size: 9.h, // 10
   );
 
   static AppBarTheme lightAppBarTheme = AppBarTheme(
