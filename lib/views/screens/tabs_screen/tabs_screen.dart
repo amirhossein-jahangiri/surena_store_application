03275ba4a6e1fec7f733eba2142239f1ff63b14b
Amirhossein-Jahangiri <amirhosseinjahangiri78@gmail.com>
2022-02-07 16:39:01 +0330

    nothing and trying to change height of bottom nav bar

--- lib/views/screens/tabs_screen/tabs_screen.dart
+++ lib/views/screens/tabs_screen/tabs_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:sizer/sizer.dart';
 import 'package:provider/provider.dart';
 
 import '/modules/provider_models/app_controller_provider_model.dart';
