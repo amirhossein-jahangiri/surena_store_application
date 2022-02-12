2b2096a82bc014d5c540b9c2b90ad03ea74b1b38
Amirhossein-Jahangiri <amirhosseinjahangiri78@gmail.com>
2022-02-12 18:43:51 +0330

    update this .dart file for leading icon size , border radius and....

---
+++ lib/views/widgets/custom_expansion_tile.dart
@@ -0,0 +1,237 @@
+import 'package:flutter/material.dart';
+
+
+// todo convert a Expansion Tile widget to a Custom Expansion Tile widget to use border radius
+// todo display custom expansion tile widget
+
+const Duration _kExpand = Duration(milliseconds: 200);
+
+class CustomExpansionTile extends StatefulWidget {
+
+  const CustomExpansionTile({
+    Key? key,
+    this.leading,
+    this.defaultLeadingIconSize,
+    required this.title,
+    this.subtitle,
+    this.onExpansionChanged,
+    this.children = const <Widget>[],
+    this.trailing,
+    this.initiallyExpanded = false,
+    this.maintainState = false,
+    this.tilePadding,
+    this.expandedCrossAxisAlignment,
+    this.expandedAlignment,
+    this.childrenPadding,
+    this.backgroundColor,
+    this.collapsedBackgroundColor,
+    this.textColor,
+    this.collapsedTextColor,
+    this.borderRadius = 0.0,
+    this.iconColor,
+    this.collapsedIconColor,
+    this.controlAffinity,
+  }) : assert(initiallyExpanded != null),
+        assert(maintainState != null),
+        assert(
+        expandedCrossAxisAlignment != CrossAxisAlignment.baseline,
+        'CrossAxisAlignment.baseline is not supported since the expanded children '
+            'are aligned in a column, not a row. Try to use another constant.',
+        ),
+        super(key: key);
+
+  final Widget? leading;
+  final double? defaultLeadingIconSize;
+  final Widget title;
+  final Widget? subtitle;
+  final ValueChanged<bool>? onExpansionChanged;
+  final List<Widget> children;
+  final Color? backgroundColor;
+  final Color? collapsedBackgroundColor;
+  final Widget? trailing;
+  final bool initiallyExpanded;
+  final bool maintainState;
+  final EdgeInsetsGeometry? tilePadding;
+  final Alignment? expandedAlignment;
+  final CrossAxisAlignment? expandedCrossAxisAlignment;
+  final EdgeInsetsGeometry? childrenPadding;
+  final double? borderRadius;
+  final Color? iconColor;
+  final Color? collapsedIconColor;
+  final Color? textColor;
+  final Color? collapsedTextColor;
+  final ListTileControlAffinity? controlAffinity;
+
+  @override
+  State<CustomExpansionTile> createState() => _CustomExpansionTileState();
+}
+
+class _CustomExpansionTileState extends State<CustomExpansionTile> with SingleTickerProviderStateMixin {
+  static final Animatable<double> _easeOutTween = CurveTween(curve: Curves.easeOut);
+  static final Animatable<double> _easeInTween = CurveTween(curve: Curves.easeIn);
+  static final Animatable<double> _halfTween = Tween<double>(begin: 0.0, end: 0.5);
+
+  final ColorTween _borderColorTween = ColorTween();
+  final ColorTween _headerColorTween = ColorTween();
+  final ColorTween _iconColorTween = ColorTween();
+  final ColorTween _backgroundColorTween = ColorTween();
+
+  late AnimationController _controller;
+  late Animation<double> _iconTurns;
+  late Animation<double> _heightFactor;
+  late Animation<Color?> _borderColor;
+  late Animation<Color?> _headerColor;
+  late Animation<Color?> _iconColor;
+  late Animation<Color?> _backgroundColor;
+
+  bool _isExpanded = false;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = AnimationController(duration: _kExpand, vsync: this);
+    _heightFactor = _controller.drive(_easeInTween);
+    _iconTurns = _controller.drive(_halfTween.chain(_easeInTween));
+    _borderColor = _controller.drive(_borderColorTween.chain(_easeOutTween));
+    _headerColor = _controller.drive(_headerColorTween.chain(_easeInTween));
+    _iconColor = _controller.drive(_iconColorTween.chain(_easeInTween));
+    _backgroundColor = _controller.drive(_backgroundColorTween.chain(_easeOutTween));
+
+    _isExpanded = PageStorage.of(context)?.readState(context) as bool? ?? widget.initiallyExpanded;
+    if (_isExpanded)
+      _controller.value = 1.0;
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  void _handleTap() {
+    setState(() {
+      _isExpanded = !_isExpanded;
+      if (_isExpanded) {
+        _controller.forward();
+      } else {
+        _controller.reverse().then<void>((void value) {
+          if (!mounted)
+            return;
+          setState(() {
+            // Rebuild without widget.children.
+          });
+        });
+      }
+      PageStorage.of(context)?.writeState(context, _isExpanded);
+    });
+    widget.onExpansionChanged?.call(_isExpanded);
+  }
+
+  // Platform or null affinity defaults to trailing.
+  ListTileControlAffinity _effectiveAffinity(ListTileControlAffinity? affinity) {
+    switch (affinity ?? ListTileControlAffinity.trailing) {
+      case ListTileControlAffinity.leading:
+        return ListTileControlAffinity.leading;
+      case ListTileControlAffinity.trailing:
+      case ListTileControlAffinity.platform:
+        return ListTileControlAffinity.trailing;
+    }
+  }
+
+  Widget? _buildIcon(BuildContext context) {
+    return RotationTransition(
+      turns: _iconTurns,
+      child: Icon(Icons.expand_more, size: widget.defaultLeadingIconSize),
+    );
+  }
+
+  Widget? _buildLeadingIcon(BuildContext context) {
+    if (_effectiveAffinity(widget.controlAffinity) != ListTileControlAffinity.leading)
+      return null;
+    return _buildIcon(context);
+  }
+
+  Widget? _buildTrailingIcon(BuildContext context) {
+    if (_effectiveAffinity(widget.controlAffinity) != ListTileControlAffinity.trailing)
+      return null;
+    return _buildIcon(context);
+  }
+
+  Widget _buildChildren(BuildContext context, Widget? child) {
+    final Color borderSideColor = _borderColor.value ?? Colors.transparent;
+
+    return Container(
+      decoration: BoxDecoration(
+        color: _backgroundColor.value ?? Colors.transparent,
+        borderRadius: BorderRadius.circular(widget.borderRadius!),
+      ),
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: <Widget>[
+          ListTileTheme.merge(
+            iconColor: _iconColor.value,
+            textColor: _headerColor.value,
+            child: ListTile(
+              onTap: _handleTap,
+              contentPadding: widget.tilePadding,
+              leading: widget.leading ?? _buildLeadingIcon(context),
+              title: widget.title,
+              subtitle: widget.subtitle,
+              trailing: widget.trailing ?? _buildTrailingIcon(context),
+            ),
+          ),
+          ClipRect(
+            child: Align(
+              alignment: widget.expandedAlignment ?? Alignment.center,
+              heightFactor: _heightFactor.value,
+              child: child,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  @override
+  void didChangeDependencies() {
+    final ThemeData theme = Theme.of(context);
+    final ColorScheme colorScheme = theme.colorScheme;
+    _borderColorTween.end = theme.dividerColor;
+    _headerColorTween
+      ..begin = widget.collapsedTextColor ?? theme.textTheme.subtitle1!.color
+      ..end = widget.textColor ?? colorScheme.primary;
+    _iconColorTween
+      ..begin = widget.collapsedIconColor ?? theme.unselectedWidgetColor
+      ..end = widget.iconColor ?? colorScheme.primary;
+    _backgroundColorTween
+      ..begin = widget.collapsedBackgroundColor
+      ..end = widget.backgroundColor;
+    super.didChangeDependencies();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final bool closed = !_isExpanded && _controller.isDismissed;
+    final bool shouldRemoveChildren = closed && !widget.maintainState;
+
+    final Widget result = Offstage(
+      offstage: closed,
+      child: TickerMode(
+        enabled: !closed,
+        child: Padding(
+          padding: widget.childrenPadding ?? EdgeInsets.zero,
+          child: Column(
+            crossAxisAlignment: widget.expandedCrossAxisAlignment ?? CrossAxisAlignment.center,
+            children: widget.children,
+          ),
+        ),
+      ),
+    );
+
+    return AnimatedBuilder(
+      animation: _controller.view,
+      builder: _buildChildren,
+      child: shouldRemoveChildren ? null : result,
+    );
+  }
+}
