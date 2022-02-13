c4a432028efa5e70008afc2d3a6f30ce93a3e151
Amirhossein-Jahangiri <amirhosseinjahangiri78@gmail.com>
2022-02-13 16:57:56 +0330

    add a user commite model for this folder

---
+++ lib/modules/models/comments_model.dart
@@ -0,0 +1,14 @@
+class CommentsModel {
+  final String? title;
+  final String? body;
+  final DateTime? time;
+  final String? userName;
+
+  const CommentsModel({
+    this.title,
+    this.body,
+    this.time,
+    this.userName,
+});
+
+}
