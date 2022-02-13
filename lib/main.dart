504465b892cd02c3b468dc7c821b14880777303e
Amirhossein-Jahangiri <amirhosseinjahangiri78@gmail.com>
2022-02-13 16:54:21 +0330

    update application title name and class name . also update provider model list add user comment provier model

--- lib/main.dart
+++ lib/main.dart
@@ -5,21 +5,25 @@ import 'package:sizer/sizer.dart';
 import '/config/theme_config.dart';
 import '/config/route_config.dart';
 import '/constants/route_constants.dart';
+import '/constants/app_constants.dart';
 import 'modules/provider_models/product_overview_screen_slider_provider_model.dart';
 import 'modules/provider_models/app_controller_provider_model.dart';
+import 'modules/provider_models/user_comments_provider_model.dart';
+
 
 void main() => runApp(
       MultiProvider(
         providers: [
-          ChangeNotifierProvider(create: (context) => ProductOverViewScreenSliderProviderModel()),
           ChangeNotifierProvider(create: (context) => AppControllerProviderModel()),
+          ChangeNotifierProvider(create: (context) => ProductOverViewScreenSliderProviderModel()),
+          ChangeNotifierProvider(create: (context) => UserCommentsProviderModel()),
         ],
-        child: const MyApp(),
+        child: const SurenaStoreApplication(),
       ),
     );
 
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+class SurenaStoreApplication extends StatelessWidget {
+  const SurenaStoreApplication({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -27,6 +31,7 @@ class MyApp extends StatelessWidget {
       // todo for sizer package and make responsive application
       builder: (context, orientation, deviceType) {
         return MaterialApp(
+          title: AppConstants.APP_TITLE,
           debugShowCheckedModeBanner: false,
           theme: ThemeConfig.lightTheme,
           initialRoute: RouteConstants.TABS_SCREEN_ROUTE_NAME,
